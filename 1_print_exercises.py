9864e4777e9149fcad3ecec060bd097d064cf013
camhar28 <100096993+camhar28@users.noreply.github.com>
2022-02-22 12:15:05 +1300

    completed print exercises

---
+++ 1_print_exercises.py
@@ -0,0 +1,32 @@
+#Quetion 1, my name
+print("Cam Harris")
+
+#Question 2, teachers name
+print("Luke Smith")
+
+#Question 3, school name
+print("Kings High School")
+
+#Question 4, my age
+print("15")
+
+#Question 5, Hours in Fav game
+print("45")
+
+#Question 6, what year will it be in 25 years
+print(2022+25)
+
+#Question 7, My age in 100 years
+print(15+100)
+
+#Question 8, 658 divided by 7
+print(658/7)
+
+#Question 9, First moon landing
+print(2022-1969)
+
+#Question 10, favorite game
+print("Rainbow Six Siege")
+
+#Question 11, Favorite class
+print("Digi Tech")
