aa89f9a7ffac7d605f1b8836bef082ad2fe7e9d8
camhar28 <100096993+camhar28@users.noreply.github.com>
2022-02-28 09:52:52 +1300

    Finished This

---
+++ 2_data_types.py
@@ -0,0 +1,60 @@
+#Question 1
+print("We will\nWe Will\nRock you")
+#Question 2
+print("\"yippekayaye mfker\" - John McClane")
+#Question 3
+print("A string is a group of words")
+#Question 4
+print("Some Numbers")
+#Question 5
+print("Some numbers with decimals")
+#Question 6
+print(543/3)
+#Question 7
+print(543//3)
+#Question 8
+print(543%3)
+#Question 9
+print(1000%9)
+#Question 10
+print(434//10)
+#Question 11
+print(3%2)
+#Question 12
+print(5%5)
+#Question 13
+print(555/2)
+#Question 14
+print(123/2)
+#Question 15
+print(123//2)
+#Question 16
+print(123%2)
+#Question 17
+print(764%2)
+#Question 18
+print(165//2)
+#Question 19
+print(13.54+46.54)
+#Question 20
+print(2+3)
+#Question 21
+print(23-17.54)
+#Question 22
+print(43.32-34)
+#Question 23
+print(32/12)
+#Question 24
+print(23*23.5)
+#Question 25
+print(45*2)
+#Question 26
+print("Cam Harris " * 10)
+#Question 27
+print("Cam Harris\n" * 10)
+#Question 28
+print("5+10=" + str(5+10))
+#Question 29
+print("15/2=" + str(15/2))
+#Question 30
+print("30/2*5=" + str(30/2*5))
---
+++ 3_Variable_Practise.py
@@ -0,0 +1,22 @@
+#Question 1
+Player_Lives = 3
+#Question 2
+Login_Name = "camhar28"
+#Question 3
+Money = 750
+#Question 4
+Phone_Number = 343 645 6480
+#Question 5 
+Name = "Cam Harris"
+#Question 6
+Age = 15
+#Question 7
+Year = 2022
+#Question 8
+print(Name)
+#Question 9 
+print(Age)
+#Question 10
+print(Year)
+#Question 11
+Surname = "Harris"
