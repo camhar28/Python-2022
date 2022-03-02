780d4004e6cadd894fb9d0ab17b327bff0a037aa
camhar28 <100096993+camhar28@users.noreply.github.com>
2022-03-03 12:36:33 +1300

    Started Exercises

---
+++ 5_exercises.py
@@ -0,0 +1,9 @@
+#Question 1 
+input("Press any key, then press enter\n")
+#Question 2
+input("Type your name, then press enter\n")
+#Question 3 
+input("Type your age, then press enter\n")
+#Question 4 
+Name = input("Type your name, then press enter\n")
+#Question 5 
